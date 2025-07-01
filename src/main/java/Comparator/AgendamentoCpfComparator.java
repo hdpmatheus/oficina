fba3d31b81c89f528aea39a4978c50cdb5e1b94c
hdpmatheus <matheuspaula051@gmail.com>
2025-06-30 23:26:10 -0300

    Update AgendamentoCpfComparator.java

--- src/main/java/Comparator/AgendamentoCpfComparator.java
+++ src/main/java/Comparator/AgendamentoCpfComparator.java
@@ -4,9 +4,26 @@ import Entidades.Agendamento;
 import java.util.Comparator;
 
 /**
- * Comparator para comparar agendamentos com base no ID do cliente.
+ * Classe que implementa um {@link Comparator} para comparar objetos do tipo {@link Agendamento}
+ * com base no ID do cliente associado ao agendamento.
+ * <p>
+ * Essa classe pode ser utilizada para ordenar listas de agendamentos em ordem crescente
+ * de acordo com o ID do cliente.
+ * </p>
+ * 
+ * @author Matheus Henrique de Paula
+ * @author Felipe Alcântara Guimarães Veloso
  */
 public class AgendamentoCpfComparator implements Comparator<Agendamento> {
+
+    /**
+     * Compara dois objetos {@link Agendamento} com base no ID do cliente.
+     *
+     * @param a1 o primeiro agendamento a ser comparado
+     * @param a2 o segundo agendamento a ser comparado
+     * @return um valor negativo se o ID do cliente de {@code a1} for menor que o de {@code a2},
+     *         zero se forem iguais, ou um valor positivo se for maior
+     */
     @Override
     public int compare(Agendamento a1, Agendamento a2) {
         return Integer.compare(a1.getIdCliente(), a2.getIdCliente());
