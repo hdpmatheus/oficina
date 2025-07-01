1ba0f5de3b5c92a650d58f2c172ccfdcff15712c
felpsveloso <felipe.veloso@ufvjm.edu.br>
2025-06-30 23:04:59 -0300

    Update Login.java

--- src/main/java/Oficina/Login.java
+++ src/main/java/Oficina/Login.java
@@ -8,12 +8,28 @@ import Controler.GerenciarGerente;
 /**
  * Classe responsável pela autenticação de login para Funcionários e Gerentes.
  * Realiza a verificação de ID e senha para acesso ao sistema da oficina.
+ * 
+ * Esta classe é utilizada no processo inicial do sistema, onde o usuário 
+ * deve se autenticar para acessar as funcionalidades disponíveis.
+ * 
+ * Autentica usuários com base no tipo (funcionário ou gerente),
+ * validando suas credenciais contra os dados armazenados.
+ * 
+ * @author 
+ * Matheus Henrique de Paula <br>
+ * Felipe Alcântara Guimarães Veloso
  */
 public class Login {
 
     private GerenciarFuncionario gerenciarFuncionario;
     private GerenciarGerente gerenciarGerente;
 
+    /**
+     * Construtor da classe Login.
+     *
+     * @param gerenciarFuncionario Instância de gerenciamento de funcionários
+     * @param gerenciarGerente Instância de gerenciamento de gerentes
+     */
     public Login(GerenciarFuncionario gerenciarFuncionario, GerenciarGerente gerenciarGerente) {
         this.gerenciarFuncionario = gerenciarFuncionario;
         this.gerenciarGerente = gerenciarGerente;
@@ -25,7 +41,7 @@ public class Login {
      * @param id   ID do usuário
      * @param senha Senha do usuário
      * @param tipo  Tipo de usuário (funcionario/gerente)
-     * @return "funcionario", "gerente" ou null se falhar
+     * @return "funcionario", "gerente" ou null se a autenticação falhar
      */
     public String realizarLogin(int id, int senha, String tipo) {
         if (tipo.equalsIgnoreCase("funcionario")) {
