a109dc2d5e52459eff64f82cac4cc2dd697060a4
felpsveloso <felipe.veloso@ufvjm.edu.br>
2025-06-30 23:07:16 -0300

    Update EstadoConfirmado.java

--- src/main/java/State/EstadoConfirmado.java
+++ src/main/java/State/EstadoConfirmado.java
@@ -2,17 +2,48 @@ package State;
 
 import Entidades.Agendamento;
 
+/**
+ * Representa o estado "Confirmado" de um {@link Agendamento}.
+ * <p>
+ * Neste estado, o agendamento pode ser:
+ * <ul>
+ *   <li>Avançado para o estado "Em Andamento"</li>
+ *   <li>Cancelado, indo para o estado "Cancelado"</li>
+ * </ul>
+ * </p>
+ *
+ * Implementa a interface {@link EstadoAgendamento} como parte do padrão State.
+ * 
+ * @author Felipe Alcantara Guimaraes Veloso
+ * @author Matheus Henrique de Paula
+ */
 public class EstadoConfirmado implements EstadoAgendamento {
+
+    /**
+     * Avança o agendamento para o estado "Em Andamento".
+     *
+     * @param agendamento o agendamento a ser alterado
+     */
     @Override
     public void avancar(Agendamento agendamento) {
         agendamento.setEstado(new EstadoEmAndamento());
     }
 
+    /**
+     * Cancela o agendamento, alterando seu estado para "Cancelado".
+     *
+     * @param agendamento o agendamento a ser alterado
+     */
     @Override
     public void cancelar(Agendamento agendamento) {
         agendamento.setEstado(new EstadoCancelado());
     }
 
+    /**
+     * Retorna o nome do estado atual.
+     *
+     * @return uma {@link String} representando o estado
+     */
     @Override
     public String getNome() {
         return "Confirmado";
