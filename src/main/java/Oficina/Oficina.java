b2a5be7ebaea30b8cbf78f1f7e553f955a27bd86
felpsveloso <felipe.veloso@ufvjm.edu.br>
2025-06-30 22:59:34 -0300

    Update Oficina.java

--- src/main/java/Oficina/Oficina.java
+++ src/main/java/Oficina/Oficina.java
@@ -13,20 +13,49 @@ import Menu.MenuGerente;
 
 import java.util.Scanner;
 
+/**
+ * Classe principal que inicia o sistema da oficina mecânica.
+ * 
+ * Esta classe é responsável por configurar os dados iniciais, realizar o login
+ * do usuário (gerente ou funcionário), direcionar para os respectivos menus
+ * e garantir a persistência dos dados ao final da execução.
+ *
+ * Funcionalidades principais:
+ * <ul>
+ *   <li>Inicialização do sistema e carregamento do estoque</li>
+ *   <li>Criação de usuários padrão (funcionário e gerente)</li>
+ *   <li>Login de acesso com autenticação</li>
+ *   <li>Abertura do menu correspondente ao tipo de usuário</li>
+ *   <li>Persistência de dados no encerramento do sistema</li>
+ * </ul>
+ * 
+ * @author
+ * Matheus Henrique de Paula <br>
+ * Felipe Alcântara Guimarães Veloso
+ */
 public class Oficina {
 
+    /**
+     * Método principal que executa o sistema da oficina mecânica.
+     *
+     * @param args Argumentos de linha de comando (não utilizados)
+     */
     public static void main(String[] args) {
         SistemaCentral central = new SistemaCentral();
         SistemaExterno sistemaExterno = new SistemaExterno();
         central.carregarEstoquePadrao();
+
+        // Configuração dos processadores de pagamento
         ProcessadorPagamento cartaoCredito = new PagamentoCartaoCredito(sistemaExterno);
         ProcessadorPagamento pix = new PagamentoPix(sistemaExterno);
 
+        // Cadastro de usuários padrão
         Funcionario funcionario1 = new Funcionario(1, 12345, "Mecanico", "Boquita", "Campo Tijuco, 45", 3890101);
         Gerente gerente1 = new Gerente(1, 5513, "Felps", "Rua Antonio Olinto, 60", 3190101);
         central.getGerenciarFuncionario().criarFuncionario(funcionario1);
         central.getGerenciarGerente().criarGerente(gerente1);
 
+        // Sistema de login
         Login loginSystem = new Login(central.getGerenciarFuncionario(), central.getGerenciarGerente());
         Scanner scanner = new Scanner(System.in);
         String tipoUsuarioLogado = null;
@@ -52,14 +81,14 @@ public class Oficina {
 
         System.out.println("✅ Acesso concedido!");
 
+        // Direciona para o menu do tipo de usuário
         if (tipoUsuarioLogado.equals("gerente")) {
             MenuGerente.menuGerente(central, scanner); 
         } else if (tipoUsuarioLogado.equals("funcionario")) {
             MenuFuncionario.menuFuncionario(central, scanner);
         }
-        
 
-        // Salvar todos os dados ao encerrar
+        // Persistência dos dados ao encerrar o sistema
         central.getGerenciarCliente().salvarCliente();
         central.getGerenciarGerente().salvarGerente();
         central.getGerenciarFuncionario().salvarFuncionario();
