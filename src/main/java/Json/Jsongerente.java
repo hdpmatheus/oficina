87dddaad13799925b525686f5f30ba0164d24a0f
felpsveloso <felipe.veloso@ufvjm.edu.br>
2025-06-30 23:14:26 -0300

    Update Jsongerente.java

--- src/main/java/Json/Jsongerente.java
+++ src/main/java/Json/Jsongerente.java
@@ -7,9 +7,25 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Classe responsável por salvar os dados de gerentes em formato JSON.
+ * Utiliza a biblioteca Gson para conversão entre objetos Java e JSON.
+ *
+ * <p>Os dados são armazenados no caminho {@code Json/JsonGerente.json}.</p>
+ *
+ * @author Felipe Alcântara Guimarães Veloso
+ * @author Matheus Henrique de Paula
+ */
 public class Jsongerente {
+
+    /** Caminho para o arquivo JSON onde os gerentes são salvos. */
     public static final String caminho = "Json/JsonGerente.json";
 
+    /**
+     * Salva uma lista de objetos {@link Gerente} no arquivo JSON definido.
+     *
+     * @param gerentes Lista de gerentes a serem salvos.
+     */
     public static void salvarGerente(List<Gerente> gerentes) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(gerentes);
