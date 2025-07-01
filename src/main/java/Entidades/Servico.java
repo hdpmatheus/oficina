f5b915c45c76c6d2e2b3dc4dd1f81c1142c3d6a8
felpsveloso <felipe.veloso@ufvjm.edu.br>
2025-06-30 23:22:16 -0300

    Update Servico.java

--- src/main/java/Entidades/Servico.java
+++ src/main/java/Entidades/Servico.java
@@ -1,10 +1,34 @@
 package Entidades;
 
+/**
+ * Representa um serviço oferecido pela oficina mecânica.
+ * 
+ * Cada serviço possui um tipo (ex: troca de óleo, alinhamento), um identificador único
+ * e um preço associado. Esta entidade é utilizada em agendamentos, controle financeiro e histórico de atendimentos.
+ * 
+ * A classe fornece métodos de acesso (getters/setters) e sobrescreve {@code toString()} para exibição descritiva.
+ * 
+ * Exemplo de uso:
+ * <pre>
+ *     Servico alinhamento = new Servico("Alinhamento", 1, 120.0);
+ * </pre>
+ * 
+ * @author
+ * Matheus Henrique de Paula <br>
+ * Felipe Alcântara Guimarães Veloso
+ */
 public class Servico {
     private String tipoServico;
     private int idServico;
     private double preco;
 
+    /**
+     * Construtor da classe Servico.
+     *
+     * @param tipoServico Descrição do tipo de serviço
+     * @param idServico Identificador único do serviço
+     * @param preco Preço cobrado pelo serviço
+     */
     public Servico(String tipoServico, int idServico, double preco) {
         this.tipoServico = tipoServico;
         this.idServico = idServico;
@@ -35,6 +59,11 @@ public class Servico {
         this.preco = preco;
     }
 
+    /**
+     * Retorna uma representação textual do serviço, com tipo, ID e preço.
+     *
+     * @return String com os dados do serviço
+     */
     @Override
     public String toString() {
         return "Servico{" +
