d9be6aef351c97ad837fe4024c8a32f602c8f897
felpsveloso <felipe.veloso@ufvjm.edu.br>
2025-06-30 23:15:38 -0300

    Update Jsonvenda.java

--- src/main/java/Json/Jsonvenda.java
+++ src/main/java/Json/Jsonvenda.java
@@ -3,13 +3,37 @@ package Json;
 import Entidades.Venda;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Classe utilitária responsável pela persistência dos dados da entidade {@link Venda} no formato JSON.
+ * 
+ * Utiliza a biblioteca Gson para serializar a lista de vendas e armazená-la em um arquivo.
+ * Atualmente, a classe possui apenas a funcionalidade de salvar os dados.
+ * 
+ * Arquivo utilizado:
+ * <ul>
+ *     <li>Json/JsonVenda.json</li>
+ * </ul>
+ * 
+ * Esta funcionalidade é usada para preservar o histórico de vendas realizadas na oficina.
+ * 
+ * @author 
+ * Matheus Henrique de Paula <br>
+ * Felipe Alcântara Guimarães Veloso
+ */
 public class Jsonvenda {
+
     public static final String caminho = "Json/JsonVenda.json";
 
+    /**
+     * Salva a lista de vendas no arquivo JSON.
+     *
+     * @param vendas Lista de vendas a ser salva
+     */
     public static void salvarVendas(List<Venda> vendas) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(vendas);
