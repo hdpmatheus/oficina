944e25531f0f3e531605a4ad3162a45f8dbf8c1d
hdpmatheus <matheuspaula051@gmail.com>
2025-06-30 23:32:16 -0300

    Update Gerente.java

--- src/main/java/Entidades/Gerente.java
+++ src/main/java/Entidades/Gerente.java
@@ -1,11 +1,26 @@
 package Entidades;
 
 /**
- * Classe que representa um gerente na oficina mecânica.
+ * Representa um gerente na oficina mecânica.
+ * <p>
+ * A classe Gerente estende a classe Funcionario, definindo o cargo como "Gerente".
+ * </p>
+ * 
+ * @author Matheus Henrique de Paula
+ * @author Felipe Alcântara Guimarães Veloso
  */
 public class Gerente extends Funcionario {
 
-    // Construtor da classe Gerente
+    /**
+     * Construtor para criar um gerente com ID, senha, nome, endereço e telefone.
+     * O cargo é automaticamente definido como "Gerente".
+     * 
+     * @param id Identificador único do gerente
+     * @param senha Senha de acesso do gerente
+     * @param nome Nome do gerente
+     * @param endereco Endereço do gerente
+     * @param telefone Telefone do gerente
+     */
     public Gerente(int id, int senha, String nome, String endereco, int telefone) {
         super(id, senha, "Gerente", nome, endereco, telefone);
     }
