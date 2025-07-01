a81bebfe3f43503cf5c5df7610c051982480b97f
hdpmatheus <matheuspaula051@gmail.com>
2025-06-30 23:29:37 -0300

    Update Acesso.java

--- src/main/java/Entidades/Acesso.java
+++ src/main/java/Entidades/Acesso.java
@@ -1,7 +1,11 @@
 package Entidades;
 
 /**
- * Representa um registro de acesso de um funcionário à oficina mecânica.
+ * Representa um registro de acesso de um funcionário à oficina mecânica,
+ * contendo informações sobre a data, horário e tipo do acesso (entrada ou saída).
+ * 
+ * @author Matheus Henrique de Paula
+ * @author Felipe Alcântara Guimarães Veloso
  */
 public class Acesso {
     private int idFuncionario;
@@ -9,7 +13,15 @@ public class Acesso {
     private Horario horario;
     private String tipo; // "Entrada" ou "Saída"
 
-    // Construtor
+    /**
+     * Construtor que inicializa um registro de acesso com os dados do funcionário,
+     * data, horário e tipo do acesso.
+     * 
+     * @param idFuncionario ID do funcionário que realizou o acesso
+     * @param data Data do acesso
+     * @param horario Horário do acesso
+     * @param tipo Tipo do acesso, podendo ser "Entrada" ou "Saída"
+     */
     public Acesso(int idFuncionario, Data data, Horario horario, String tipo) {
         this.idFuncionario = idFuncionario;
         this.data = data;
@@ -17,27 +29,56 @@ public class Acesso {
         this.tipo = tipo;
     }
 
-    // Métodos get
+    /**
+     * Retorna o ID do funcionário.
+     * 
+     * @return ID do funcionário
+     */
     public int getIdFuncionario() {
         return idFuncionario;
     }
 
+    /**
+     * Retorna a data do acesso.
+     * 
+     * @return Data do acesso
+     */
     public Data getData() {
         return data;
     }
 
+    /**
+     * Retorna o horário do acesso.
+     * 
+     * @return Horário do acesso
+     */
     public Horario getHorario() {
         return horario;
     }
 
+    /**
+     * Retorna o tipo do acesso, que pode ser "Entrada" ou "Saída".
+     * 
+     * @return Tipo do acesso
+     */
     public String getTipo() {
         return tipo;
     }
 
+    /**
+     * Retorna a data e horário concatenados em uma única string.
+     * 
+     * @return Data e horário do acesso no formato string
+     */
     public String getDataHora() {
         return data.toString() + " " + horario.toString();
     }
 
+    /**
+     * Retorna uma representação em string do objeto Acesso.
+     * 
+     * @return String com os dados do acesso
+     */
     @Override
     public String toString() {
         return "Acesso{" + "idFuncionario=" + idFuncionario + ", data=" + data + ", horario=" + horario + ", tipo=" + tipo + '}';
