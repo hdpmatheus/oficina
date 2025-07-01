8a1d8aecec7e5c5e7f01182e883d8f1961854184
felpsveloso <felipe.veloso@ufvjm.edu.br>
2025-06-30 23:13:37 -0300

    Update Jsonfuncionario.java

--- src/main/java/Json/Jsonfuncionario.java
+++ src/main/java/Json/Jsonfuncionario.java
@@ -3,13 +3,37 @@ package Json;
 import Entidades.Funcionario;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Classe utilitária responsável pela persistência de dados de {@link Funcionario} no formato JSON.
+ * 
+ * Utiliza a biblioteca Gson para serializar e salvar a lista de funcionários em um arquivo.
+ * Essa funcionalidade garante que os dados dos funcionários sejam preservados entre execuções do sistema.
+ * 
+ * Arquivo utilizado:
+ * <ul>
+ *     <li>Json/JsonFuncionario.json</li>
+ * </ul>
+ * 
+ * Esta classe não possui método de leitura, apenas de gravação dos dados no momento atual.
+ * 
+ * @author 
+ * Matheus Henrique de Paula <br>
+ * Felipe Alcântara Guimarães Veloso
+ */
 public class Jsonfuncionario {
+
     public static final String caminho = "Json/JsonFuncionario.json";
 
+    /**
+     * Salva a lista de funcionários no arquivo JSON especificado.
+     *
+     * @param funcionarios Lista de funcionários a ser salva
+     */
     public static void salvarFuncionario(List<Funcionario> funcionarios) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(funcionarios);
